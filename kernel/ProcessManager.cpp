b4cc91f12290a9956391c1c1760b50ce665b0983
Arnold Lev <aalev@cpp.edu>
2022-10-24 20:41:08 -0700

    Final Renice

--- kernel/ProcessManager.cpp
+++ kernel/ProcessManager.cpp
@@ -421,9 +421,8 @@ ProcessManager::Result ProcessManager::dequeueProcess(Process *proc, const bool 
 {
     if (m_scheduler->dequeue(proc, ignoreState) != Scheduler::Success)
     {
-        //ERROR("process ID " << proc->getID() << " not removed from Scheduler");
-        //return IOError;
-        return Success;
+        ERROR("process ID " << proc->getID() << " not removed from Scheduler");
+        return IOError;
     }
 
     return Success;
